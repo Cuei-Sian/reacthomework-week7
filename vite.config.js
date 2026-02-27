83dc1726b848a023cf122053c086400891e546a2
doreen <doreen20080615@gmail.com>
2026-02-27 23:37:43 +0800

    新增：初始化

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/reacthomework-week7/',
+  plugins: [react()],
+  server: {
+    open: true, // 加這一行，run dev才會自動開網頁
+  },
+});
