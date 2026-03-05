aab15ec1a493bfb7f06b98b21d561e9652db1c99
doreen <doreen20080615@gmail.com>
2026-03-06 00:17:59 +0800

    新增：彈出視窗

---
+++ src/components/MessageToast.jsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+
+//共用元件，吐司的彈跳視窗，因為每個視窗都會用到，所以建立在App中作連結設定
+function MessageToast() {
+  const messages = useSelector((state) => state.message); //是陣列
+
+  return (
+    <div className="toast-container position-fixed top-0 end-0 p-3">
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className="toast show"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+        >
+          <div className={`toast-header text-white bg-${message.type}`}>
+            <strong className="me-auto">{message.title}</strong>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="toast"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="toast-body">{message.text}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MessageToast;
--- src/slice/messageSlice.js
+++ src/slice/messageSlice.js
@@ -1,13 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-// 定義狀態怎麼改（slice）
+// slice：定義狀態怎麼改
 export const messageSlice = createSlice({
   name: 'message',
-  initialState: [],
+  initialState: [
+    // {
+    //   id: 1,
+    //   type: 'success',
+    //   title: '成功',
+    //   text: 'test',
+    // },
+  ],
   reducers: {
-    createMessage(state, action) {},
-    removeMessage(state, action) {},
+    createMessage(state, action) {
+      state.push({
+        id: action.payload.id,
+        type: action.payload.success ? 'success' : 'danger',
+        title: action.payload.success ? '成功' : '失敗',
+        text: action.payload.message,
+      });
+    },
+    removeMessage(state, action) {
+      const index = state.findIndex((message) => message.id === action.payload);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
   },
 });
+
+// 建立Message兩秒之後會自動消失的功能
+export const createAsyncMessage = createAsyncThunk(
+  'message/createAsyncMessage',
+  async (payload, { dispatch, requestId }) => {
+    dispatch(
+      createMessage({
+        ...payload,
+        id: requestId,
+      }),
+    );
+    setTimeout(() => {
+      dispatch(removeMessage(requestId));
+    }, 2000);
+  },
+);
+
+export const { createMessage, removeMessage } = messageSlice.actions;
 
 export default messageSlice.reducer;
